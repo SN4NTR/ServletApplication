5cf0a1d905e0ba2647ac92d54b10a096be797569
Aliaksandr Miron <aliaksandr.miron@LeverX.local>
2019-11-09 22:00:07 +0300

    using Stream API for returning type

--- src/main/java/com/company/servletapp/service/UserServiceImpl.java
+++ src/main/java/com/company/servletapp/service/UserServiceImpl.java
@@ -6,50 +6,42 @@ import com.company.servletapp.repository.UserRepositoryImpl;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService {
 
-    private final static Gson GSON = new Gson();
+    private final static Gson gson = new Gson();
 
     private UserRepository userRepository = new UserRepositoryImpl();
 
     @Override
     public void save(BufferedReader reader) {
-        User user = GSON.fromJson(reader, User.class);
+        User user = gson.fromJson(reader, User.class);
         userRepository.save(user);
     }
 
     @Override
     public List<String> getByFirstName(String firstName) {
-        List<User> users = userRepository.getByFirstName(firstName);
-        List<String> usersToJson = new ArrayList<>();
-
-        for (User u : users) {
-            usersToJson.add(GSON.toJson(u));
-        }
-
-        return usersToJson;
+        return userRepository.getByFirstName(firstName)
+                .stream()
+                .map(gson::toJson)
+                .collect(Collectors.toList());
     }
 
     @Override
     public String getById(String id) {
         User user = userRepository.getById(Integer.parseInt(id));
 
-        return GSON.toJson(user);
+        return gson.toJson(user);
     }
 
     @Override
     public List<String> getAll() {
-        List<User> users = userRepository.getAll();
-        List<String> usersToJson = new ArrayList<>();
-
-        for (User u : users) {
-            usersToJson.add(GSON.toJson(u));
-        }
-
-        return usersToJson;
+        return userRepository.getAll()
+                .stream()
+                .map(gson::toJson)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -59,7 +51,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void update(BufferedReader reader, String id) {
-        User user = GSON.fromJson(reader, User.class);
+        User user = gson.fromJson(reader, User.class);
         user.setId(Integer.parseInt(id));
         userRepository.update(user);
     }
