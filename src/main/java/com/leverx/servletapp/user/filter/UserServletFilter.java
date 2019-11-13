0cfa906a7330c2b09de681da28ec6526d43239cb
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-13 15:57:55 +0300

    created constants for variables

--- src/main/java/com/leverx/servletapp/user/filter/UserServletFilter.java
+++ src/main/java/com/leverx/servletapp/user/filter/UserServletFilter.java
@@ -11,11 +11,14 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @WebFilter(filterName = "UserServletFilter", urlPatterns = {"/users", "/users/*"})
 public class UserServletFilter implements Filter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServletFilter.class.getSimpleName());
+
+    private static final String CONTENT_TYPE = "application/json";
 
     @Override
     public void init(FilterConfig filterConfig) {
@@ -24,9 +27,9 @@ public class UserServletFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setCharacterEncoding("UTF-8");
-        servletResponse.setCharacterEncoding("UTF-8");
-        servletResponse.setContentType("application/json");
+        servletRequest.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        servletResponse.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        servletResponse.setContentType(CONTENT_TYPE);
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
