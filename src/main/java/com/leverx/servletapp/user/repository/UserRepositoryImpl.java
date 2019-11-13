6a39fa74f45e2bb4d240533f3c9eb81c379b9ab0
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-13 12:07:08 +0300

    added new method for optimizing code

--- src/main/java/com/leverx/servletapp/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/user/repository/UserRepositoryImpl.java
@@ -17,28 +17,15 @@ public class UserRepositoryImpl implements UserRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryImpl.class.getSimpleName());
 
-    private static final String TABLE_NAME = "users";
-    private static final String ID_COLUMN = "id";
-    private static final String FIRST_NAME_COLUMN = "first_name";
-
-    private static final String SQL_INSERT = "INSERT INTO " + TABLE_NAME + " (" + FIRST_NAME_COLUMN + ") VALUES (?)";
-    private static final String SQL_SELECT_BY_FIRST_NAME = "SELECT * FROM " + TABLE_NAME + " WHERE " + FIRST_NAME_COLUMN + " = ?";
-    private static final String SQL_SELECT_BY_ID = "SELECT * FROM " + TABLE_NAME + " WHERE " + ID_COLUMN + " = ?";
-    private static final String SQL_SELECT_ALL = "SELECT * FROM " + TABLE_NAME;
-    private static final String SQL_DELETE = "DELETE FROM " + TABLE_NAME + " WHERE " + ID_COLUMN + " = ?";
-    private static final String SQL_UPDATE = "UPDATE " + TABLE_NAME + " SET " + FIRST_NAME_COLUMN + " = ? WHERE " + ID_COLUMN + " = ?";
-
     @Override
     public void save(User user) {
         LOGGER.info("Saving user with name '{}'.", user.getFirstName());
 
         try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection != null ? connection.prepareStatement(SQL_INSERT) : null) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL.INSERT)) {
 
-            if (preparedStatement != null) {
-                preparedStatement.setString(1, user.getFirstName());
-                preparedStatement.executeUpdate();
-            }
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.executeUpdate();
 
             LOGGER.info("User has been saved");
         } catch (SQLException ex) {
@@ -48,96 +35,67 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Collection<User> findByFirstName(String firstName) {
-        List<User> users = new ArrayList<>();
-
         LOGGER.info("Getting user by firstName = {}", firstName);
 
         try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection != null ? connection.prepareStatement(SQL_SELECT_BY_FIRST_NAME) : null) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL.SELECT_BY_FIRST_NAME)) {
 
-            if (preparedStatement != null) {
-                preparedStatement.setString(1, firstName);
+            preparedStatement.setString(1, firstName);
 
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    while (resultSet != null && resultSet.next()) {
-                        String userId = resultSet.getString(ID_COLUMN);
-                        String userFirstName = resultSet.getString(FIRST_NAME_COLUMN);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                List<User> users = new ArrayList<>();
 
-                        User user = new User();
-                        user.setFirstName(userFirstName);
-                        user.setId(Integer.parseInt(userId));
-
-                        users.add(user);
-                    }
+                while (resultSet.next()) {
+                    User user = getUserFromResultSet(resultSet);
+                    users.add(user);
                 }
 
-                LOGGER.info("User has been found");
+                return users;
             }
         } catch (SQLException ex) {
             LOGGER.error("SQL State: {}\n{}", ex.getSQLState(), ex.getMessage());
+            return null;
         }
-
-        return users;
     }
 
     @Override
     public User findById(int id) {
-        User user = new User();
-
         LOGGER.info("Getting user by id = {}", id);
 
         try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection != null ? connection.prepareStatement(SQL_SELECT_BY_ID) : null) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL.SELECT_BY_ID)) {
 
-            if (preparedStatement != null) {
-                preparedStatement.setInt(1, id);
+            preparedStatement.setInt(1, id);
 
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    while (resultSet != null && resultSet.next()) {
-                        String userId = resultSet.getString(ID_COLUMN);
-                        String userFirstName = resultSet.getString(FIRST_NAME_COLUMN);
-
-                        user.setId(Integer.parseInt(userId));
-                        user.setFirstName(userFirstName);
-                    }
-
-                    LOGGER.info("User has been found");
-                }
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next() ? getUserFromResultSet(resultSet) : null;
             }
         } catch (SQLException ex) {
             LOGGER.error("SQL State: {}\n{}", ex.getSQLState(), ex.getMessage());
+            return null;
         }
-
-        return user;
     }
 
     @Override
     public Collection<User> findAll() {
-        List<User> users = new ArrayList<>();
-
         LOGGER.info("Getting all users");
 
         try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection != null ? connection.prepareStatement(SQL_SELECT_ALL) : null;
-             ResultSet resultSet = preparedStatement != null ? preparedStatement.executeQuery() : null) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL.SELECT_ALL);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while (resultSet != null && resultSet.next()) {
-                String id = resultSet.getString(ID_COLUMN);
-                String firstName = resultSet.getString(FIRST_NAME_COLUMN);
+            List<User> users = new ArrayList<>();
 
-                User user = new User();
-                user.setId(Integer.parseInt(id));
-                user.setFirstName(firstName);
-
+            while (resultSet.next()) {
+                User user = getUserFromResultSet(resultSet);
                 users.add(user);
             }
 
-            LOGGER.info("Users are got");
+            return users;
         } catch (SQLException ex) {
             LOGGER.error("SQL State: {}\n{}", ex.getSQLState(), ex.getMessage());
+            return null;
         }
-
-        return users;
     }
 
     @Override
@@ -145,12 +103,10 @@ public class UserRepositoryImpl implements UserRepository {
         LOGGER.info("Deleting user with id = {}", id);
 
         try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection != null ? connection.prepareStatement(SQL_DELETE) : null) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL.DELETE)) {
 
-            if (preparedStatement != null) {
-                preparedStatement.setInt(1, id);
-                preparedStatement.executeUpdate();
-            }
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
 
             LOGGER.info("User has been deleted");
         } catch (SQLException ex) {
@@ -163,17 +119,24 @@ public class UserRepositoryImpl implements UserRepository {
         LOGGER.info("Updating user with id = {}", user.getId());
 
         try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection != null ? connection.prepareStatement(SQL_UPDATE) : null) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL.UPDATE)) {
 
-            if (preparedStatement != null) {
-                preparedStatement.setString(1, user.getFirstName());
-                preparedStatement.setInt(2, user.getId());
-                preparedStatement.executeUpdate();
-            }
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setInt(2, user.getId());
+            preparedStatement.executeUpdate();
 
             LOGGER.info("User has been updated");
         } catch (SQLException ex) {
             LOGGER.error("SQL State: {}\n{}", ex.getSQLState(), ex.getMessage());
         }
+    }
+
+    private User getUserFromResultSet(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt(SQL.ID);
+        String firstName = resultSet.getString(SQL.FIRST_NAME);
+
+        LOGGER.info("User with id = {} has been found", id);
+
+        return new User(id, firstName);
     }
 }
