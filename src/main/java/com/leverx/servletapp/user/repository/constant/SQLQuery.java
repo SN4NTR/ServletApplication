99eb8ffbc7170326d98601628a54add16fdc2f2f
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-14 09:43:09 +0300

    renamed class, column constants moved to another class

--- src/main/java/com/leverx/servletapp/user/repository/constant/SQLQuery.java
+++ src/main/java/com/leverx/servletapp/user/repository/constant/SQLQuery.java
@@ -1,13 +1,10 @@
-package com.leverx.servletapp.user.constant;
+package com.leverx.servletapp.user.repository.constant;
 
-public class SQL {
+public class SQLQuery {
 
-    public static final String ID = "id";
-    public static final String FIRST_NAME = "first_name";
-
-    public static final String INSERT = "INSERT INTO users (" + FIRST_NAME + ") VALUES (?)";
-    public static final String SELECT_BY_ID = "SELECT * FROM users WHERE " + ID + " = ?";
+    public static final String INSERT = "INSERT INTO users (first_name) VALUES (?)";
+    public static final String SELECT_BY_ID = "SELECT * FROM users WHERE id = ?";
     public static final String SELECT_ALL = "SELECT * FROM users";
-    public static final String DELETE = "DELETE FROM users WHERE " + ID + " = ?";
-    public static final String UPDATE = "UPDATE users SET " + FIRST_NAME + " = ? WHERE " + ID + " = ?";
+    public static final String DELETE = "DELETE FROM users WHERE id = ?";
+    public static final String UPDATE = "UPDATE users SET firs_name = ? WHERE id = ?";
 }
