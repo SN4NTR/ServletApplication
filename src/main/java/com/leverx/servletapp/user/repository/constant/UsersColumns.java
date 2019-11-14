9806b3112ccc56c02bd613d33a91f144473e11b5
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-14 09:42:09 +0300

    created class for constants

---
+++ src/main/java/com/leverx/servletapp/user/repository/constant/UsersColumns.java
@@ -0,0 +1,7 @@
+package com.leverx.servletapp.user.repository.constant;
+
+public class UsersColumns {
+
+    public static final String ID = "id";
+    public static final String FIRST_NAME = "first_name";
+}
