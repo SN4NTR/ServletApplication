b3b1f353d7d45c7765bf887b0c4057cbcba94a05
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-14 23:02:32 +0300

    fixed syntax mistake

--- src/main/java/com/leverx/servletapp/user/repository/constant/SQLQuery.java
+++ src/main/java/com/leverx/servletapp/user/repository/constant/SQLQuery.java
@@ -6,5 +6,5 @@ public class SQLQuery {
     public static final String SELECT_BY_ID = "SELECT * FROM users WHERE id = ?";
     public static final String SELECT_ALL = "SELECT * FROM users";
     public static final String DELETE = "DELETE FROM users WHERE id = ?";
-    public static final String UPDATE = "UPDATE users SET firs_name = ? WHERE id = ?";
+    public static final String UPDATE = "UPDATE users SET first_name = ? WHERE id = ?";
 }
