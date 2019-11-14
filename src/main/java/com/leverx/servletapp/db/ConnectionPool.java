b741740ddf32eef3d5a08368957e6d06729ab602
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-14 19:50:56 +0300

    created ConnectionPool

---
+++ src/main/java/com/leverx/servletapp/db/ConnectionPool.java
@@ -0,0 +1,83 @@
+package com.leverx.servletapp.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.InternalServerErrorException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public final class ConnectionPool implements AutoCloseable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionPool.class.getSimpleName());
+
+    private static ConnectionPool connectionPool;
+
+    private static final int MAX_CONNECTIONS = 10;
+    private static final int ELEMENT_INDEX = 0;
+
+    private final List<Connection> unusedConnections = new LinkedList<>();
+    private final List<Connection> usedConnections = new LinkedList<>();
+
+    private ConnectionPool() {
+        while (unusedConnections.size() < MAX_CONNECTIONS) {
+            Connection connection = createConnection();
+            unusedConnections.add(connection);
+        }
+    }
+
+    public static ConnectionPool getInstance() {
+        if (connectionPool == null) {
+            connectionPool = new ConnectionPool();
+        }
+
+        return connectionPool;
+    }
+
+    private static Connection createConnection() {
+        LOGGER.info("Trying to create connection to database");
+
+        var driverClassName = DataBaseProperties.driverClassName;
+        var dataBaseUrl = DataBaseProperties.dataBaseUrl;
+        var username = DataBaseProperties.username;
+        var password = DataBaseProperties.password;
+
+        try {
+            Class.forName(driverClassName);
+            Connection connection = DriverManager.getConnection(dataBaseUrl, username, password);
+
+            LOGGER.info("Connection has been created");
+
+            return connection;
+        } catch (SQLException ex) {
+            LOGGER.error("SQL State: {}\n{}", ex.getSQLState(), ex.getMessage());
+            throw new InternalServerErrorException();
+        } catch (ClassNotFoundException ex) {
+            LOGGER.error(ex.getMessage());
+            throw new InternalServerErrorException();
+        }
+    }
+
+    public Connection getConnection() {
+        while (unusedConnections.isEmpty()) {
+            LOGGER.info("All connections are busy");
+        }
+
+        Connection connection = unusedConnections.get(ELEMENT_INDEX);
+        unusedConnections.remove(ELEMENT_INDEX);
+        usedConnections.add(connection);
+
+        return connection;
+    }
+
+    @Override
+    public void close() {
+        int lastElementIndex = usedConnections.size() - 1;
+        Connection connection = usedConnections.get(lastElementIndex);
+        usedConnections.remove(lastElementIndex);
+        unusedConnections.add(connection);
+    }
+}
--- src/main/java/com/leverx/servletapp/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/user/repository/UserRepositoryImpl.java
@@ -1,6 +1,6 @@
 package com.leverx.servletapp.user.repository;
 
-import com.leverx.servletapp.db.JdbcConnection;
+import com.leverx.servletapp.db.ConnectionPool;
 import com.leverx.servletapp.user.entity.User;
 import com.leverx.servletapp.user.entity.UserDto;
 import org.slf4j.Logger;
@@ -21,6 +21,7 @@ import static com.leverx.servletapp.user.repository.constant.SQLQuery.UPDATE;
 import static com.leverx.servletapp.user.repository.constant.UsersColumns.FIRST_NAME;
 import static com.leverx.servletapp.user.repository.constant.UsersColumns.ID;
 
+//TODO for all method create PoolConnection
 public class UserRepositoryImpl implements UserRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryImpl.class.getSimpleName());
@@ -32,7 +33,9 @@ public class UserRepositoryImpl implements UserRepository {
     public void save(UserDto user) {
         LOGGER.info("Saving user with name '{}'.", user.getFirstName());
 
-        try (var connection = JdbcConnection.getConnection();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+
+        try (var connection = connectionPool.getConnection();
              var preparedStatement = connection.prepareStatement(INSERT)) {
 
             preparedStatement.setString(ID_INDEX, user.getFirstName());
@@ -49,7 +52,9 @@ public class UserRepositoryImpl implements UserRepository {
     public User findById(int id) {
         LOGGER.info("Getting user by id = {}", id);
 
-        try (var connection = JdbcConnection.getConnection();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+
+        try (var connection = connectionPool.getConnection();
              var preparedStatement = connection.prepareStatement(SELECT_BY_ID)) {
 
             preparedStatement.setInt(ID_INDEX, id);
@@ -67,8 +72,8 @@ public class UserRepositoryImpl implements UserRepository {
     public Collection<User> findAll() {
         LOGGER.info("Getting all users");
 
-        try (var connection = JdbcConnection.getConnection();
-             var preparedStatement = connection.prepareStatement(SELECT_ALL);
+        try (var connectionPool = ConnectionPool.getInstance();
+             var preparedStatement = connectionPool.getConnection().prepareStatement(SELECT_ALL);
              var resultSet = preparedStatement.executeQuery()) {
 
             return getListOfUsersFromResultSet(resultSet);
@@ -82,7 +87,9 @@ public class UserRepositoryImpl implements UserRepository {
     public void delete(int id) {
         LOGGER.info("Deleting user with id = {}", id);
 
-        try (var connection = JdbcConnection.getConnection();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+
+        try (var connection = connectionPool.getConnection();
              var preparedStatement = connection.prepareStatement(DELETE)) {
 
             preparedStatement.setInt(ID_INDEX, id);
@@ -99,7 +106,9 @@ public class UserRepositoryImpl implements UserRepository {
     public void update(int id, UserDto user) {
         LOGGER.info("Updating user with id = {}", id);
 
-        try (var connection = JdbcConnection.getConnection();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+
+        try (var connection = connectionPool.getConnection();
              var preparedStatement = connection.prepareStatement(UPDATE)) {
 
             preparedStatement.setString(ID_INDEX, user.getFirstName());
