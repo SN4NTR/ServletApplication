2bb337874397a5983b389b2b04fab22245fbdf0a
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-15 11:04:38 +0300

    deleted unused import

--- src/main/java/com/leverx/servletapp/listener/RequestListener.java
+++ src/main/java/com/leverx/servletapp/listener/RequestListener.java
@@ -3,7 +3,6 @@ package com.leverx.servletapp.listener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.annotation.WebListener;
