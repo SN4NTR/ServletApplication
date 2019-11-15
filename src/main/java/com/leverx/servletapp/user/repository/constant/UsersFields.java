e4f5cc4c5d65dbeb130c1bbe094d86a0f00eb4fe
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-15 15:29:31 +0300

    renamed class

--- src/main/java/com/leverx/servletapp/user/repository/constant/UsersFields.java
+++ src/main/java/com/leverx/servletapp/user/repository/constant/UsersFields.java
@@ -1,6 +1,6 @@
 package com.leverx.servletapp.user.repository.constant;
 
-public class UsersColumns {
+public class UsersFields {
 
     public static final String ID = "id";
     public static final String FIRST_NAME = "first_name";
