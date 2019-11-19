d33a17f2de164bdf517c80ac0ac76d9a37dfa49d
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-20 01:12:41 +0300

    added command for creation of table of cats

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(60) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS cats (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    owner_id INT NOT NULL,
+    FOREIGN KEY (owner_id) references users (id)
+);
