9cfac2bf89cb3b45105f9332522dc7031cb51413
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-20 10:57:10 +0300

    added new annotation

--- src/main/java/com/leverx/servletapp/user/entity/User.java
+++ src/main/java/com/leverx/servletapp/user/entity/User.java
@@ -1,9 +1,12 @@
 package com.leverx.servletapp.user.entity;
 
 import com.leverx.servletapp.cat.entity.Cat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,7 +15,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.List;
@@ -20,21 +22,22 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "users")
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class User {
 
     @Id
     @Column
+    @NonNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column
+    @NonNull
     private String firstName;
 
     @OneToMany(fetch = FetchType.LAZY,
             cascade = CascadeType.REMOVE,
             mappedBy = "owner")
-    @JoinColumn
     private List<Cat> cats;
 }
