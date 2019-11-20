48beb9e0528d5773e7b5b3b98e580cfd7ca562cc
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-20 18:03:46 +0300

    created save method

--- src/main/java/com/leverx/servletapp/cat/service/CatServiceImpl.java
+++ src/main/java/com/leverx/servletapp/cat/service/CatServiceImpl.java
@@ -2,15 +2,34 @@ package com.leverx.servletapp.cat.service;
 
 import com.leverx.servletapp.cat.entity.Cat;
 import com.leverx.servletapp.cat.entity.CatDto;
+import com.leverx.servletapp.cat.mapper.CatMapper;
+import com.leverx.servletapp.cat.repository.CatRepository;
+import com.leverx.servletapp.cat.repository.CatRepositoryImpl;
 import com.leverx.servletapp.user.entity.User;
 
 import java.util.Collection;
 
+import static com.leverx.servletapp.validator.EntityValidator.isEntityValid;
+
 public class CatServiceImpl implements CatService {
 
-    @Override
-    public void save(CatDto cat) {
+    private CatRepository catRepository = new CatRepositoryImpl();
 
+    private static final int NAME_LENGTH_MIN = 5;
+    private static final int NAME_LENGTH_MAX = 60;
+
+    @Override
+    public void save(CatDto catDto) {
+        if (isEntityValid(catDto)) {
+            var cat = CatMapper.catDtoToCat(catDto);
+            catRepository.save(cat);
+        } else {
+            String message = String.format(
+                    "Length of name must be between %s and %s" +
+                    "\nor date of birth can't be bigger than today's date.",
+                    NAME_LENGTH_MIN, NAME_LENGTH_MAX);
+            throw new IllegalArgumentException(message);
+        }
     }
 
     @Override
