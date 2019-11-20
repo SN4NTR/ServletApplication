56fd133703a0f1ec516723393f775fb6ba4d4a32
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-20 18:03:23 +0300

    added validation annotations

--- src/main/java/com/leverx/servletapp/cat/entity/CatDto.java
+++ src/main/java/com/leverx/servletapp/cat/entity/CatDto.java
@@ -2,8 +2,18 @@ package com.leverx.servletapp.cat.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+import java.sql.Date;
+
 @Data
 public class CatDto {
 
+    @NotNull
+    @Size(min = 5, max = 60)
     private String name;
+
+    @PastOrPresent
+    private Date dateOfBirth;
 }
--- src/main/java/com/leverx/servletapp/user/entity/UserDto.java
+++ src/main/java/com/leverx/servletapp/user/entity/UserDto.java
@@ -2,8 +2,13 @@ package com.leverx.servletapp.user.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserDto {
 
+    @NotNull
+    @Size(min = 5, max = 60)
     private String firstName;
 }
