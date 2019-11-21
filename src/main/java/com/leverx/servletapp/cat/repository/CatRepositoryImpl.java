eaa7e9bf8853116c80f910abdad0fb46d7040f71
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-21 17:23:58 +0300

    added ability to create and retrieve cats

--- src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
@@ -2,19 +2,42 @@ package com.leverx.servletapp.cat.repository;
 
 import com.leverx.servletapp.cat.entity.Cat;
 import com.leverx.servletapp.user.entity.User;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
+import static com.leverx.servletapp.db.HibernateConfig.getSessionFactory;
+
 public class CatRepositoryImpl implements CatRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CatRepositoryImpl.class.getSimpleName());
+
+    private SessionFactory sessionFactory = getSessionFactory();
 
     @Override
     public void save(Cat cat) {
+        LOGGER.info("Saving cat with name '{}'.", cat.getName());
 
+        var session = sessionFactory.openSession();
+        var transaction = session.beginTransaction();
+        session.save(cat);
+        transaction.commit();
+        session.close();
+
+        LOGGER.info("Cat is saved");
     }
 
     @Override
     public Cat findById(int id) {
-        return null;
+        LOGGER.info("Getting cat with id = {}", id);
+
+        var session = sessionFactory.openSession();
+        var cat = session.get(Cat.class, id);
+        session.close();
+
+        return cat;
     }
 
     @Override
@@ -24,6 +47,13 @@ public class CatRepositoryImpl implements CatRepository {
 
     @Override
     public Collection<Cat> findAll() {
-        return null;
+        LOGGER.info("Getting all users");
+
+        var session = sessionFactory.openSession();
+        var query = session.createQuery("from Cat");
+        var cats = query.list();
+        session.close();
+
+        return cats;
     }
 }
