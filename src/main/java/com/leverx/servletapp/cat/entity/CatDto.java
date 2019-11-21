dd8beacaba85fae5f2fad79bfd4193bbc6366277
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-21 17:20:19 +0300

    changed date type

--- src/main/java/com/leverx/servletapp/cat/entity/CatDto.java
+++ src/main/java/com/leverx/servletapp/cat/entity/CatDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
-import java.sql.Date;
+import java.util.Date;
 
 @Data
 public class CatDto {
@@ -14,6 +14,7 @@ public class CatDto {
     @Size(min = 5, max = 60)
     private String name;
 
+    @NotNull
     @PastOrPresent
     private Date dateOfBirth;
 }
