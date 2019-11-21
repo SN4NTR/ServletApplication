00b3906251c7f8f96330b1a9e026faa778422eba
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-21 17:24:22 +0300

    created servlet methods

--- src/main/java/com/leverx/servletapp/cat/servlet/CatServlet.java
+++ src/main/java/com/leverx/servletapp/cat/servlet/CatServlet.java
@@ -8,6 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.leverx.servletapp.cat.mapper.CatMapper.collectionToJson;
+import static com.leverx.servletapp.cat.mapper.CatMapper.jsonToCatDto;
+import static com.leverx.servletapp.cat.mapper.CatMapper.readJsonBody;
+import static javax.servlet.http.HttpServletResponse.SC_CREATED;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
 public class CatServlet extends HttpServlet {
 
     private CatService catService = new CatServiceImpl();
@@ -15,11 +21,22 @@ public class CatServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         var printWriter = resp.getWriter();
+        var cats = catService.findAll();
+        var result = collectionToJson(cats);
+        printWriter.print(result);
 
+        printWriter.flush();
+        resp.setStatus(SC_OK);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        var reader = req.getReader();
+        var jsonBody = readJsonBody(reader);
+        var catDto = jsonToCatDto(jsonBody);
 
+        catService.save(catDto);
+
+        resp.setStatus(SC_CREATED);
     }
 }
