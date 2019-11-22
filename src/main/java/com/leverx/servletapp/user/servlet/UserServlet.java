31e0b4d71175b7d7c0fef7eec58e83f2dd954c27
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-23 00:01:13 +0300

    modified method's logic

--- src/main/java/com/leverx/servletapp/user/servlet/UserServlet.java
+++ src/main/java/com/leverx/servletapp/user/servlet/UserServlet.java
@@ -13,11 +13,11 @@ import static com.leverx.servletapp.user.mapper.UserMapper.collectionToJson;
 import static com.leverx.servletapp.user.mapper.UserMapper.jsonToUserDto;
 import static com.leverx.servletapp.user.mapper.UserMapper.readJsonBody;
 import static com.leverx.servletapp.user.mapper.UserMapper.userToJson;
-import static com.leverx.servletapp.user.servlet.util.UserServletUtils.PATH;
-import static com.leverx.servletapp.user.servlet.util.UserServletUtils.getIdFromUrl;
-import static com.leverx.servletapp.user.servlet.util.UserServletUtils.getValueFromUrl;
+import static com.leverx.servletapp.util.ServletUtils.getIdFromUrl;
+import static com.leverx.servletapp.util.ServletUtils.getValueFromUrl;
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import static javax.servlet.http.HttpServletResponse.SC_CREATED;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 import static org.apache.commons.lang3.math.NumberUtils.isParsable;
@@ -25,6 +25,8 @@ import static org.apache.commons.lang3.math.NumberUtils.isParsable;
 public class UserServlet extends HttpServlet {
 
     private final UserService userService = new UserServiceImpl();
+
+    private static final String PATH = "users";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -35,14 +37,12 @@ public class UserServlet extends HttpServlet {
         var value = getValueFromUrl(urlToString);
 
         if (PATH.equals(value)) {
-            printAllUsers(printWriter);
+            printAllUsers(printWriter, resp);
         } else if (isParsable(value)) {
-            printUserById(printWriter, value);
+            printUserById(printWriter, value, resp);
         } else {
-            printUserByFirstName(printWriter, value);
+            printUserByFirstName(printWriter, value, resp);
         }
-
-        resp.setStatus(SC_OK);
         printWriter.flush();
     }
 
@@ -51,9 +51,7 @@ public class UserServlet extends HttpServlet {
         var reader = req.getReader();
         var jsonBody = readJsonBody(reader);
         var userDto = jsonToUserDto(jsonBody);
-
         userService.save(userDto);
-
         resp.setStatus(SC_CREATED);
     }
 
@@ -65,7 +63,6 @@ public class UserServlet extends HttpServlet {
 
         if (idOpt.isPresent()) {
             var id = idOpt.get();
-
             userService.delete(id);
             resp.setStatus(SC_NO_CONTENT);
         } else {
@@ -78,13 +75,11 @@ public class UserServlet extends HttpServlet {
         var url = req.getRequestURL();
         var urlToString = url.toString();
         var idOpt = getIdFromUrl(urlToString);
-
         if (idOpt.isPresent()) {
             var id = idOpt.get();
             var reader = req.getReader();
             var jsonBody = readJsonBody(reader);
             var userDto = jsonToUserDto(jsonBody);
-
             userService.update(id, userDto);
             resp.setStatus(SC_OK);
         } else {
@@ -92,22 +87,33 @@ public class UserServlet extends HttpServlet {
         }
     }
 
-    private void printUserByFirstName(PrintWriter printWriter, String value) {
+    private void printUserByFirstName(PrintWriter printWriter, String value, HttpServletResponse resp) {
         var users = userService.findByName(value);
-        var result = collectionToJson(users);
-        printWriter.print(result);
+        if (!users.isEmpty()) {
+            var result = collectionToJson(users);
+            printWriter.print(result);
+            resp.setStatus(SC_OK);
+        } else {
+            resp.setStatus(SC_NOT_FOUND);
+        }
     }
 
-    private void printUserById(PrintWriter printWriter, String value) {
+    private void printUserById(PrintWriter printWriter, String value, HttpServletResponse resp) {
         var id = Integer.parseInt(value);
         var user = userService.findById(id);
-        var result = userToJson(user);
-        printWriter.print(result);
+        if (user != null) {
+            var result = userToJson(user);
+            printWriter.print(result);
+            resp.setStatus(SC_OK);
+        } else {
+            resp.setStatus(SC_NOT_FOUND);
+        }
     }
 
-    private void printAllUsers(PrintWriter printWriter) {
+    private void printAllUsers(PrintWriter printWriter, HttpServletResponse resp) {
         var users = userService.findAll();
         var result = collectionToJson(users);
         printWriter.print(result);
+        resp.setStatus(SC_OK);
     }
 }
