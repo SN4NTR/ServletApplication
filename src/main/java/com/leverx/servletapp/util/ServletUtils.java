d3bed1935af3d9048205315ab623286ba475a4c4
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-23 00:01:45 +0300

    class moved to the project's root

--- src/main/java/com/leverx/servletapp/util/ServletUtils.java
+++ src/main/java/com/leverx/servletapp/util/ServletUtils.java
@@ -1,4 +1,4 @@
-package com.leverx.servletapp.user.servlet.util;
+package com.leverx.servletapp.util;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -8,22 +8,18 @@ import java.util.Optional;
 import static org.apache.commons.lang3.math.NumberUtils.isParsable;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class UserServletUtils {
+public final class ServletUtils {
 
     private static final String DELIMITER = "/";
-
-    public static final String PATH = "users";
 
     public static String getValueFromUrl(String url) {
         var urlComponents = url.split(DELIMITER);
         var lastElementIndex = urlComponents.length - 1;
-
         return urlComponents[lastElementIndex];
     }
 
     public static Optional<Integer> getIdFromUrl(String url) {
         var value = getValueFromUrl(url);
-
         return isParsable(value) ? Optional.of(Integer.parseInt(value)) : Optional.empty();
     }
 }
