fe297c7aa24cdc9258eaf76ad158a5bbf18e4822
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-23 19:18:19 +0300

    deleted unused classes

--- src/main/java/com/leverx/servletapp/user/repository/constant/UsersFields.java
+++
@@ -1,7 +0,0 @@
-package com.leverx.servletapp.user.repository.constant;
-
-public class UsersFields {
-
-    public static final String ID = "id";
-    public static final String FIRST_NAME = "first_name";
-}
