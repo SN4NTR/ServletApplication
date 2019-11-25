36318a4c31d116e5e0e914dccaf0f226aa815690
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-25 22:10:05 +0300

    added new annotation

--- src/main/java/com/leverx/servletapp/cat/mapper/CatMapper.java
+++ src/main/java/com/leverx/servletapp/cat/mapper/CatMapper.java
@@ -2,11 +2,14 @@ package com.leverx.servletapp.cat.mapper;
 
 import com.leverx.servletapp.cat.entity.Cat;
 import com.leverx.servletapp.cat.entity.CatDto;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
+import static lombok.AccessLevel.PRIVATE;
 
+@NoArgsConstructor(access = PRIVATE)
 public class CatMapper {
 
     public static Cat catDtoToCat(CatDto catDto) {
--- src/main/java/com/leverx/servletapp/user/mapper/UserMapper.java
+++ src/main/java/com/leverx/servletapp/user/mapper/UserMapper.java
@@ -2,14 +2,14 @@ package com.leverx.servletapp.user.mapper;
 
 import com.leverx.servletapp.user.entity.User;
 import com.leverx.servletapp.user.entity.UserDto;
-import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
+import static lombok.AccessLevel.PRIVATE;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = PRIVATE)
 public final class UserMapper {
 
     public static User userDtoToUser(UserDto userDto) {
