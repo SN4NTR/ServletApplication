d1402712a4c02e8c728c0292746f552e06c63d96
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-25 22:12:48 +0300

    local variables changed to var

--- src/main/java/com/leverx/servletapp/db/HibernateConfig.java
+++ src/main/java/com/leverx/servletapp/db/HibernateConfig.java
@@ -5,19 +5,16 @@ import com.leverx.servletapp.user.entity.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
-import java.util.Properties;
 
 public class HibernateConfig {
 
     public static SessionFactory getSessionFactory() {
-        Configuration configuration = new Configuration();
+        var configuration = new Configuration();
         configuration.addAnnotatedClass(User.class);
         configuration.addAnnotatedClass(Cat.class);
 
-        Properties configProperties = configuration.getProperties();
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+        var configProperties = configuration.getProperties();
+        var serviceRegistry = new StandardServiceRegistryBuilder()
                 .applySettings(configProperties)
                 .build();
 
