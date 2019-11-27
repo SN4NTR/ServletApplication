08747b797117fdc8216ea2c9a42860c49f860c45
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-27 17:45:02 +0300

    added singleton pattern

--- src/main/java/com/leverx/servletapp/db/HibernateConfig.java
+++ src/main/java/com/leverx/servletapp/db/HibernateConfig.java
@@ -2,22 +2,31 @@ package com.leverx.servletapp.db;
 
 import com.leverx.servletapp.cat.entity.Cat;
 import com.leverx.servletapp.user.entity.User;
+import lombok.NoArgsConstructor;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
 public class HibernateConfig {
 
-    public static SessionFactory getSessionFactory() {
-        var configuration = new Configuration();
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Cat.class);
+    private static SessionFactory sessionFactory;
 
-        var configProperties = configuration.getProperties();
-        var serviceRegistry = new StandardServiceRegistryBuilder()
-                .applySettings(configProperties)
-                .build();
+    public static synchronized SessionFactory getInstance() {
+        if (sessionFactory == null) {
+            var configuration = new Configuration();
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Cat.class);
 
-        return configuration.buildSessionFactory(serviceRegistry);
+            var configProperties = configuration.getProperties();
+            var serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configProperties)
+                    .build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
     }
 }
