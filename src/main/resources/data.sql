f2d01c44b85507221c9d9cfbf4a2f0d55b7ae64b
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-28 14:33:49 +0300

    modified sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,13 @@
-CREATE DATABASE IF NOT EXISTS db;
-
 USE db;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users
+(
+    id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(60) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS cats (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    owner_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS cats
+(
+    id       INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(60) NOT NULL,
+    owner_id INT         NOT NULL,
     FOREIGN KEY (owner_id) references users (id)
 );
