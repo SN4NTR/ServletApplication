7659f1e118835ddbac56a4cef9573d84f95f7d0b
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-28 13:11:51 +0300

    added ability to rollback

--- src/main/java/com/leverx/servletapp/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/user/repository/UserRepositoryImpl.java
@@ -1,12 +1,14 @@
 package com.leverx.servletapp.user.repository;
 
 import com.leverx.servletapp.user.entity.User;
-import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 
+import javax.ws.rs.InternalServerErrorException;
 import java.util.Collection;
 
 import static com.leverx.servletapp.db.HibernateConfig.getSessionFactory;
+import static java.util.Objects.nonNull;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserRepositoryImpl implements UserRepository {
@@ -16,82 +18,114 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public void save(User user) {
         LOGGER.info("Saving user with name '{}'.", user.getFirstName());
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var transaction = session.beginTransaction();
-        session.save(user);
-        transaction.commit();
-        session.close();
+        var sessionFactory = getSessionFactory();
+        Transaction transaction = null;
 
-        LOGGER.info("User was saved");
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+
+            LOGGER.info("User was saved");
+        } catch (Exception ex) {
+            if (nonNull(transaction)) {
+                transaction.rollback();
+                LOGGER.error("User can't be saved");
+                throw new InternalServerErrorException(ex);
+            }
+        }
     }
 
     @Override
     public User findById(int id) {
         LOGGER.info("Getting user by id = {}", id);
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var user = session.get(User.class, id);
-        session.close();
+        var sessionFactory = getSessionFactory();
 
-        return user;
+        try (var session = sessionFactory.openSession()) {
+            return session.get(User.class, id);
+        } catch (Exception ex) {
+            LOGGER.error("User can't be found");
+            throw new InternalServerErrorException(ex);
+        }
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public Collection<User> findByName(String name) {
         LOGGER.info("Getting user by firstName = {}", name);
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var query = session.createQuery("from User where firstName=:firstName");
-        query.setParameter("firstName", name);
-        var users = query.list();
-        session.close();
+        var sessionFactory = getSessionFactory();
 
-        return users;
+        try (var session = sessionFactory.openSession()) {
+            var query = session.createQuery("from User where firstName=:firstName");
+            query.setParameter("firstName", name);
+            return query.list();
+        } catch (Exception ex) {
+            LOGGER.error("User can't be found");
+            throw new InternalServerErrorException(ex);
+        }
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public Collection<User> findAll() {
         LOGGER.info("Getting all users");
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var query = session.createQuery("from User");
-        var users = query.list();
-        session.close();
+        var sessionFactory = getSessionFactory();
 
-        return users;
+        try (var session = sessionFactory.openSession()) {
+            var query = session.createQuery("from User");
+            return query.list();
+        } catch (Exception ex) {
+            LOGGER.error("Users can't be found");
+            throw new InternalServerErrorException(ex);
+        }
     }
 
     @Override
     public void delete(User user) {
         var id = user.getId();
         LOGGER.info("Deleting user with id = {}", id);
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var transaction = session.beginTransaction();
-        session.delete(user);
-        transaction.commit();
-        session.close();
+        var sessionFactory = getSessionFactory();
+        Transaction transaction = null;
+
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(user);
+            transaction.commit();
+
+            LOGGER.info("User with id = {} was deleted", id);
+        } catch (Exception ex) {
+            if (nonNull(transaction)) {
+                transaction.rollback();
+                LOGGER.error("User can't be deleted");
+                throw  new InternalServerErrorException(ex);
+            }
+        }
     }
 
     @Override
     public void update(User user) {
-        LOGGER.info("Updating user with id = {}", user.getId());
-        SessionFactory sessionFactory = getSessionFactory();
+        var id = user.getId();
+        LOGGER.info("Updating user with id = {}", id);
 
-        var session = sessionFactory.openSession();
-        var transaction = session.beginTransaction();
-        session.update(user);
-        transaction.commit();
-        session.close();
+        var sessionFactory = getSessionFactory();
+        Transaction transaction = null;
 
-        LOGGER.info("User is updated");
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.update(user);
+            transaction.commit();
+            LOGGER.info("User is updated");
+        } catch (Exception ex) {
+            if (nonNull(transaction)) {
+                transaction.rollback();
+                LOGGER.error("User can't be updated");
+                throw  new InternalServerErrorException(ex);
+            }
+        }
     }
 }
