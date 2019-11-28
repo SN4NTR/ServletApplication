e4657f7f86b35e06a8c4ac677be5f255f5e0748c
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-28 14:32:55 +0300

    added ability to rollback

--- src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
@@ -1,12 +1,14 @@
 package com.leverx.servletapp.cat.repository;
 
 import com.leverx.servletapp.cat.entity.Cat;
-import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 
+import javax.ws.rs.InternalServerErrorException;
 import java.util.Collection;
 
 import static com.leverx.servletapp.db.HibernateConfig.getSessionFactory;
+import static java.util.Objects.nonNull;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class CatRepositoryImpl implements CatRepository {
@@ -16,40 +18,52 @@ public class CatRepositoryImpl implements CatRepository {
     @Override
     public void save(Cat cat) {
         LOGGER.info("Saving cat with name '{}'.", cat.getName());
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var transaction = session.beginTransaction();
-        session.save(cat);
-        transaction.commit();
-        session.close();
+        var sessionFactory = getSessionFactory();
+        Transaction transaction = null;
 
-        LOGGER.info("Cat was saved");
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(cat);
+            transaction.commit();
+
+            LOGGER.info("Cat was saved");
+        } catch (Exception ex) {
+            if (nonNull(transaction)) {
+                transaction.rollback();
+                LOGGER.error("Cat can't be saved");
+                throw new InternalServerErrorException(ex);
+            }
+        }
     }
 
     @Override
     public Cat findById(int id) {
         LOGGER.info("Getting cat with id = {}", id);
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var cat = session.get(Cat.class, id);
-        session.close();
+        var sessionFactory = getSessionFactory();
 
-        return cat;
+        try (var session = sessionFactory.openSession()) {
+            return session.get(Cat.class, id);
+        } catch (Exception ex) {
+            LOGGER.error("Cat with id = {} can't be found", id);
+            throw new InternalServerErrorException(ex);
+        }
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public Collection<Cat> findAll() {
         LOGGER.info("Getting all users");
-        SessionFactory sessionFactory = getSessionFactory();
 
-        var session = sessionFactory.openSession();
-        var query = session.createQuery("from Cat");
-        var cats = query.list();
-        session.close();
+        var sessionFactory = getSessionFactory();
 
-        return cats;
+        try (var session = sessionFactory.openSession()) {
+            var query = session.createQuery("from Cat");
+            return query.list();
+        } catch (Exception ex) {
+            LOGGER.error("Cats can't be found");
+            throw new InternalServerErrorException(ex);
+        }
     }
 }
