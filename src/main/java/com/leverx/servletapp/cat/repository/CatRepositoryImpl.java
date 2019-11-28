4ab57844034e2a17f9d905ed55624e16bdae31f8
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-28 18:49:18 +0300

    added EntityManager

--- src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
@@ -1,13 +1,14 @@
 package com.leverx.servletapp.cat.repository;
 
 import com.leverx.servletapp.cat.entity.Cat;
-import org.hibernate.Transaction;
 import org.slf4j.Logger;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.ws.rs.InternalServerErrorException;
 import java.util.Collection;
 
-import static com.leverx.servletapp.db.HibernateConfig.getSessionFactory;
+import static com.leverx.servletapp.db.HibernateConfig.getEntityManagerFactory;
 import static java.util.Objects.nonNull;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -19,20 +20,28 @@ public class CatRepositoryImpl implements CatRepository {
     public void save(Cat cat) {
         LOGGER.info("Saving cat with name '{}'.", cat.getName());
 
-        var sessionFactory = getSessionFactory();
-        Transaction transaction = null;
+        var entityManagerFactory = getEntityManagerFactory();
+        EntityTransaction transaction = null;
+        EntityManager entityManager = null;
 
-        try (var session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            session.save(cat);
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            entityManager.persist(cat);
             transaction.commit();
 
             LOGGER.info("Cat was saved");
         } catch (Exception ex) {
             if (nonNull(transaction)) {
                 transaction.rollback();
-                LOGGER.error("Cat can't be saved");
-                throw new InternalServerErrorException(ex);
+            }
+            LOGGER.error("Cat can't be saved");
+            throw new InternalServerErrorException(ex);
+        } finally {
+            if (nonNull(entityManager)) {
+                entityManager.close();
             }
         }
     }
@@ -41,13 +50,30 @@ public class CatRepositoryImpl implements CatRepository {
     public Cat findById(int id) {
         LOGGER.info("Getting cat with id = {}", id);
 
-        var sessionFactory = getSessionFactory();
+        var entityManagerFactory = getEntityManagerFactory();
+        EntityTransaction transaction = null;
+        EntityManager entityManager = null;
 
-        try (var session = sessionFactory.openSession()) {
-            return session.get(Cat.class, id);
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            var cat = entityManager.find(Cat.class, id);
+            transaction.commit();
+            LOGGER.info("Cat with id = {} was found", id);
+
+            return cat;
         } catch (Exception ex) {
+            if (nonNull(transaction)) {
+                transaction.rollback();
+            }
             LOGGER.error("Cat with id = {} can't be found", id);
             throw new InternalServerErrorException(ex);
+        } finally {
+            if (nonNull(entityManager)) {
+                entityManager.close();
+            }
         }
     }
 
@@ -56,14 +82,31 @@ public class CatRepositoryImpl implements CatRepository {
     public Collection<Cat> findAll() {
         LOGGER.info("Getting all users");
 
-        var sessionFactory = getSessionFactory();
+        var entityManagerFactory = getEntityManagerFactory();
+        EntityTransaction transaction = null;
+        EntityManager entityManager = null;
 
-        try (var session = sessionFactory.openSession()) {
-            var query = session.createQuery("from Cat");
-            return query.list();
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            var query = entityManager.createQuery("from Cat");
+            var cats = query.getResultList();
+            transaction.commit();
+            LOGGER.info("Cats were found");
+
+            return cats;
         } catch (Exception ex) {
+            if (nonNull(transaction)) {
+                transaction.rollback();
+            }
             LOGGER.error("Cats can't be found");
             throw new InternalServerErrorException(ex);
+        } finally {
+            if (nonNull(entityManager)) {
+                entityManager.close();
+            }
         }
     }
 }
