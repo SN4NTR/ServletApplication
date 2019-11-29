a734f24716ffaf330d90ea6d401017fe27488bb8
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-29 19:01:10 +0300

    created metamodel for Cat

---
+++ src/main/java/com/leverx/servletapp/cat/entity/meta/Cat_.java
@@ -0,0 +1,11 @@
+package com.leverx.servletapp.cat.entity.meta;
+
+import com.leverx.servletapp.cat.entity.Cat;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Cat.class)
+public abstract class Cat_ {
+}
