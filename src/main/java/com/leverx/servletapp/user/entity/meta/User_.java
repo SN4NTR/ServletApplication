8ca73df76e7a83912b5014ef722bfebfb0e1b508
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-11-29 18:55:29 +0300

    deleted unused field

--- src/main/java/com/leverx/servletapp/user/entity/meta/User_.java
+++ src/main/java/com/leverx/servletapp/user/entity/meta/User_.java
@@ -1,18 +1,13 @@
 package com.leverx.servletapp.user.entity.meta;
 
-
 import com.leverx.servletapp.user.entity.User;
 
 import javax.annotation.processing.Generated;
-import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
-
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(User.class)
 public abstract class User_ {
-
-    public static volatile SingularAttribute<User, String> firstName;
 
     public static final String FIRST_NAME = "firstName";
 }
