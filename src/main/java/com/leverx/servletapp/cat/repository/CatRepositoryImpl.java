70db172d60290a8f14d0b5fb523aef3f5ddd9027
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-01 00:41:33 +0300

    method's refactoring

--- src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
+++ src/main/java/com/leverx/servletapp/cat/repository/CatRepositoryImpl.java
@@ -3,10 +3,12 @@ package com.leverx.servletapp.cat.repository;
 import com.leverx.servletapp.cat.entity.Cat;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.persistence.EntityTransaction;
 import javax.ws.rs.InternalServerErrorException;
 import java.util.Collection;
 
 import static com.leverx.servletapp.db.HibernateConfig.getEntityManager;
+import static java.util.Objects.nonNull;
 
 @Slf4j
 public class CatRepositoryImpl implements CatRepository {
@@ -16,18 +18,18 @@ public class CatRepositoryImpl implements CatRepository {
         log.info("Saving cat with name '{}'.", cat.getName());
 
         var entityManager = getEntityManager();
-        var transaction = entityManager.getTransaction();
+        EntityTransaction transaction = null;
 
         try {
+            transaction = entityManager.getTransaction();
             transaction.begin();
-            entityManager.persist(cat);
-            transaction.commit();
 
+            entityManager.persist(cat);
+
+            transaction.commit();
             log.info("Cat was saved");
         } catch (Exception ex) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
+            rollbackTransaction(transaction);
             log.error("Cat can't be saved");
             throw new InternalServerErrorException(ex);
         } finally {
@@ -40,19 +42,19 @@ public class CatRepositoryImpl implements CatRepository {
         log.info("Getting cat with id = {}", id);
 
         var entityManager = getEntityManager();
-        var transaction = entityManager.getTransaction();
+        EntityTransaction transaction = null;
 
         try {
+            transaction = entityManager.getTransaction();
             transaction.begin();
-            var cat = entityManager.find(Cat.class, id);
-            transaction.commit();
 
+            var cat = entityManager.find(Cat.class, id);
+
+            transaction.commit();
             log.info("Cat with id = {} was found", id);
             return cat;
         } catch (Exception ex) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
+            rollbackTransaction(transaction);
             log.error("Cat with id = {} can't be found", id);
             throw new InternalServerErrorException(ex);
         } finally {
@@ -65,7 +67,7 @@ public class CatRepositoryImpl implements CatRepository {
         log.info("Getting all users");
 
         var entityManager = getEntityManager();
-        var transaction = entityManager.getTransaction();
+        EntityTransaction transaction = null;
 
         try {
             var criteriaBuilder = entityManager.getCriteriaBuilder();
@@ -74,21 +76,27 @@ public class CatRepositoryImpl implements CatRepository {
 
             criteriaQuery.select(root);
 
+            transaction = entityManager.getTransaction();
             transaction.begin();
+
             var query = entityManager.createQuery(criteriaQuery);
             var cats = query.getResultList();
-            transaction.commit();
 
+            transaction.commit();
             log.info("Cats were found");
             return cats;
         } catch (Exception ex) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
+            rollbackTransaction(transaction);
             log.error("Cats can't be found");
             throw new InternalServerErrorException(ex);
         } finally {
             entityManager.close();
+        }
+    }
+
+    private void rollbackTransaction(EntityTransaction transaction) {
+        if (nonNull(transaction) && transaction.isActive()) {
+            transaction.rollback();
         }
     }
 }
