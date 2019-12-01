e82df1b26ec2f67d8de5efcec39d6309a023805b
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-02 00:23:32 +0300

    modified dtos

--- src/main/java/com/leverx/servletapp/user/entity/UserDto.java
+++ src/main/java/com/leverx/servletapp/user/entity/UserDto.java
@@ -1,17 +1,34 @@
 package com.leverx.servletapp.user.entity;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.leverx.servletapp.cat.entity.CatDto;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
-@Data
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
+
+@Getter
+@Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class UserDto {
+
+    private int id;
 
     @NotNull
     @Size(min = 5, max = 60)
     private String firstName;
 
+    @JsonInclude(NON_EMPTY)
+    private List<CatDto> cats;
+
+    @JsonProperty(access = WRITE_ONLY)
     private List<Integer> catsIdList;
 }
