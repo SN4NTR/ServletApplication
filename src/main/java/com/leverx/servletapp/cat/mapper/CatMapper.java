c0c8ecc386b5cde6e28d711ab7e24817645f1eee
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-02 11:14:53 +0300

    modified CatMapper

--- src/main/java/com/leverx/servletapp/cat/mapper/CatMapper.java
+++ src/main/java/com/leverx/servletapp/cat/mapper/CatMapper.java
@@ -4,6 +4,11 @@ import com.leverx.servletapp.cat.entity.Cat;
 import com.leverx.servletapp.cat.entity.CatDto;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static com.leverx.servletapp.user.mapper.UserMapper.userToDto;
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
@@ -13,5 +18,38 @@ public final class CatMapper {
         var name = catDto.getName();
         var dateOfBirth = catDto.getDateOfBirth();
         return new Cat(name, dateOfBirth);
+    }
+
+    public static List<CatDto> catCollectionToDtoList(Collection<Cat> cats) {
+        var catDtoList = new ArrayList<CatDto>();
+
+        for (var cat : cats) {
+            var catDto = catToDto(cat);
+            catDtoList.add(catDto);
+        }
+        return catDtoList;
+    }
+
+    public static CatDto catToDtoWithOwner(Cat cat) {
+        var owner = cat.getOwner();
+        var ownerDto = userToDto(owner);
+
+        var catDto = catToDto(cat);
+        catDto.setOwner(ownerDto);
+
+        return catDto;
+    }
+
+    private static CatDto catToDto(Cat cat) {
+        var id = cat.getId();
+        var name = cat.getName();
+        var dateOfBirth = cat.getDateOfBirth();
+
+        var catDto = new CatDto();
+        catDto.setId(id);
+        catDto.setName(name);
+        catDto.setDateOfBirth(dateOfBirth);
+
+        return catDto;
     }
 }
