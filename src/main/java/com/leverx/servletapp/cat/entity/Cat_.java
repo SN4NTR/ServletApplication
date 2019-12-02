e127c77b1bdc1897d483a71567fa271b2f8fc17d
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-02 11:55:36 +0300

    modified meta for Cat class

--- src/main/java/com/leverx/servletapp/cat/entity/Cat_.java
+++ src/main/java/com/leverx/servletapp/cat/entity/Cat_.java
@@ -1,9 +1,12 @@
 package com.leverx.servletapp.cat.entity;
 
 import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Cat.class)
 public abstract class Cat_ {
+
+    public static volatile SingularAttribute<Cat, Integer> id;
 }
