9ffec96886c85a59cfd981a68d3133982cf2b3ca
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-02 11:15:28 +0300

    modified fields

--- src/main/java/com/leverx/servletapp/user/entity/User_.java
+++ src/main/java/com/leverx/servletapp/user/entity/User_.java
@@ -1,12 +1,14 @@
 package com.leverx.servletapp.user.entity;
 
 import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(User.class)
 public abstract class User_ {
 
-    public static final String FIRST_NAME = "firstName";
+    public static volatile SingularAttribute<User, Integer> id;
+    public static volatile SingularAttribute<User, String> firstName;
 }
 
