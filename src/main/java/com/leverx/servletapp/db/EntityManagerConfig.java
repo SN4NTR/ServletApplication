43d999cd579b7309e7104f49e2ce12a0e384400a
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-03 20:32:36 +0300

    renamed variable

--- src/main/java/com/leverx/servletapp/db/EntityManagerConfig.java
+++ src/main/java/com/leverx/servletapp/db/EntityManagerConfig.java
@@ -11,14 +11,14 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor(access = PRIVATE)
 public class EntityManagerConfig {
 
-    private static final String persistenceUnitName = "JpaPersistence";
-    private static final EntityManagerFactory entityManagerFactory;
+    private static final String PERSISTENCE_UNIT_NAME = "JpaPersistence";
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
 
     static {
-        entityManagerFactory = createEntityManagerFactory(persistenceUnitName);
+        ENTITY_MANAGER_FACTORY = createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
     }
 
     public static synchronized EntityManager getEntityManager() {
-        return entityManagerFactory.createEntityManager();
+        return ENTITY_MANAGER_FACTORY.createEntityManager();
     }
 }
