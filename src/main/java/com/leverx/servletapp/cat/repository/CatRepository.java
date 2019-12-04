2243c11dd4bc3b63520b0d79a3c2371be2326549
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-04 22:26:08 +0300

    modified CatRepository's methods

--- src/main/java/com/leverx/servletapp/cat/repository/CatRepository.java
+++ src/main/java/com/leverx/servletapp/cat/repository/CatRepository.java
@@ -3,12 +3,15 @@ package com.leverx.servletapp.cat.repository;
 import com.leverx.servletapp.cat.entity.Cat;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface CatRepository {
 
     void save(Cat cat);
 
-    Cat findById(int id);
+    Optional<Cat> findById(int id);
+
+    Collection<Cat> findByOwnerId(int id);
 
     Collection<Cat> findAll();
 }
