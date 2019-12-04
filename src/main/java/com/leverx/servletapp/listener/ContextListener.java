94ad856fec8ff9fc1a443b7f26a556bb3dc83b9b
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-04 22:30:13 +0300

    added creating of EntityManager on context starting and closing on context stopping

--- src/main/java/com/leverx/servletapp/listener/ContextListener.java
+++ src/main/java/com/leverx/servletapp/listener/ContextListener.java
@@ -5,16 +5,23 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import static com.leverx.servletapp.db.EntityManagerConfig.closeEntityManagerFactory;
+import static com.leverx.servletapp.db.EntityManagerConfig.createEntityManagerFactory;
+
 @Slf4j
 public class ContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         log.info("Context initialized.");
+        createEntityManagerFactory();
+        log.info("EntityManagerFactory is created");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         log.info("Context destroyed.");
+        closeEntityManagerFactory();
+        log.info("EntityManagerFactory is closed");
     }
 }
