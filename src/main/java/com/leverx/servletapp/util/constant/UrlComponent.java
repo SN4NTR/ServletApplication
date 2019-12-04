26e2afa2751ff917cd224f97e7521fb179828ad6
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-04 23:37:44 +0300

    created constants for utils

---
+++ src/main/java/com/leverx/servletapp/util/constant/UrlComponent.java
@@ -0,0 +1,8 @@
+package com.leverx.servletapp.util.constant;
+
+public class UrlComponent {
+
+    public static final String DELIMITER = "/";
+    public static final int LAST_ELEMENT_POSITION = 1;
+    public static final int PENULTIMATE_ELEMENT_POSITION = 2;
+}
