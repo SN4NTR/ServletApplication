122c7234d731943002ed73b6c28009d3e21414d2
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-04 22:27:23 +0300

    moved methods to cat layer

--- src/main/java/com/leverx/servletapp/user/service/UserService.java
+++ src/main/java/com/leverx/servletapp/user/service/UserService.java
@@ -1,26 +1,22 @@
 package com.leverx.servletapp.user.service;
 
-import com.leverx.servletapp.cat.entity.Cat;
-import com.leverx.servletapp.cat.entity.CatDto;
-import com.leverx.servletapp.user.entity.UserDto;
+import com.leverx.servletapp.user.entity.dto.UserInputDto;
+import com.leverx.servletapp.user.entity.dto.UserOutputDto;
+import com.leverx.servletapp.user.entity.dto.UserWithCatsDto;
 
 import java.util.Collection;
 
 public interface UserService {
 
-    void save(UserDto user);
-
-    UserDto findById(int id);
-
-    Collection<UserDto> findByName(String name);
-
-    Collection<UserDto> findAll();
-
-    Collection<CatDto> findCatsByUserId(int id);
+    void save(UserInputDto user);
 
     void delete(int id);
 
-    void assignCat(int userId, CatDto catDto);
+    void update(int id, UserInputDto user);
 
-    void update(int id, UserDto user);
+    UserWithCatsDto findById(int id);
+
+    Collection<UserOutputDto> findByName(String name);
+
+    Collection<UserOutputDto> findAll();
 }
