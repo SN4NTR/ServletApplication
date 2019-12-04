8297d7dfcf6612e1cffd6b289df7a45cee8be181
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-04 22:29:01 +0300

    added close method

--- src/main/java/com/leverx/servletapp/db/EntityManagerConfig.java
+++ src/main/java/com/leverx/servletapp/db/EntityManagerConfig.java
@@ -4,21 +4,25 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-import static javax.persistence.Persistence.createEntityManagerFactory;
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public class EntityManagerConfig {
 
     private static final String PERSISTENCE_UNIT_NAME = "JpaPersistence";
-    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
+    private static EntityManagerFactory ENTITY_MANAGER_FACTORY;
 
-    static {
-        ENTITY_MANAGER_FACTORY = createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    public static void createEntityManagerFactory() {
+        ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
     }
 
-    public static synchronized EntityManager getEntityManager() {
+    public static EntityManager getEntityManager() {
         return ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
+
+    public static void closeEntityManagerFactory() {
+        ENTITY_MANAGER_FACTORY.close();
     }
 }
