770c3a410d3211dcf1e625d086b6f874a0480e2a
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-05 12:29:53 +0300

    created new exception

---
+++ src/main/java/com/leverx/servletapp/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.leverx.servletapp.exception;
+
+public class EntityNotFoundException extends Exception {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
