6d859bec1ab271ccfa741502fbec2d491b12dd05
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-10 11:49:17 +0300

    created class with response statuses

---
+++ src/main/java/com/leverx/servletapp/constant/HttpResponseStatus.java
@@ -0,0 +1,15 @@
+package com.leverx.servletapp.constant;
+
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class HttpResponseStatus {
+
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int NO_CONTENT = 204;
+    public static final int UNPROCESSABLE_ENTITY = 422;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+}
