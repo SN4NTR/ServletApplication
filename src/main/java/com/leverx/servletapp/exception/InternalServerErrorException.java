77646ecdda895426336eccc51f485d3340133579
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-12 13:58:53 +0300

    added no args constructor

--- src/main/java/com/leverx/servletapp/exception/InternalServerErrorException.java
+++ src/main/java/com/leverx/servletapp/exception/InternalServerErrorException.java
@@ -1,8 +1,10 @@
 package com.leverx.servletapp.exception;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class InternalServerErrorException extends RuntimeException {
 
     private int responseStatus;
