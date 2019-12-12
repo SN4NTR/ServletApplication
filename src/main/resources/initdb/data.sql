9b2b76c25b8faad2d61a1c8cbfaaa681bdb407ea
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-12 17:50:49 +0300

    updated sql script

--- src/main/resources/initdb/data.sql
+++ src/main/resources/initdb/data.sql
@@ -5,11 +5,26 @@ CREATE TABLE IF NOT EXISTS users
     id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(60) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS cats
+CREATE TABLE IF NOT EXISTS animals
 (
     id            INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name          VARCHAR(60) NOT NULL,
-    date_of_birth DATE        NOT NULL,
-    owner_id      INT         NULL,
-    FOREIGN KEY (owner_id) references users (id)
+    date_of_birth DATE        NOT NULL
+);
+CREATE TABLE IF NOT EXISTS owner_animal
+(
+    owner_id  INT NULL,
+    animal_id INT NULL,
+    foreign key (owner_id) references users (id),
+    foreign key (animal_id) references animals (id)
+);
+CREATE TABLE IF NOT EXISTS cats
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    foreign key (id) references animals (id)
+);
+CREATE TABLE IF NOT EXISTS dogs
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    foreign key (id) references animals (id)
 );
