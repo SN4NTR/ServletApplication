38f0a7ab92201a610543f3508a0bfb1fc8bc7399
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-18 10:18:23 +0300

    created enum for method's type

---
+++ src/main/java/com/leverx/servletapp/util/constant/MethodType.java
@@ -0,0 +1,9 @@
+package com.leverx.servletapp.util.constant;
+
+public enum MethodType {
+    GET_ALL_USERS,
+    GET_USERS_CATS,
+    GET_USERS_DOGS,
+    GET_USERS_ANIMALS,
+    GET_USER_BY_ATTRIBUTE
+}
