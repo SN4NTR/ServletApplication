d608daaca66983bacd10d83d7c5a8c5f3f496787
Aliaksandr Miron <alex.miron1999@gmail.com>
2019-12-23 11:48:41 +0300

    updated PropertyLoader

--- src/main/java/com/leverx/servletapp/db/PropertyLoader.java
+++ src/main/java/com/leverx/servletapp/db/PropertyLoader.java
@@ -5,18 +5,19 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public class PropertyLoader {
+class PropertyLoader {
 
     private static final String DB_URL_ENV_NAME = "DB_URL";
     private static final String USER_ENV_NAME = "DB_LOGIN";
     private static final String PASSWORD_ENV_NAME = "DB_PASSWORD";
 
     private static final String URL_PROPERTY_NAME = "javax.persistence.jdbc.url";
+    private static final String URL_PARAMETERS = "?createDatabaseIfNotExist=true&useUnicode=yes&characterEncoding=UTF-8";
     private static final String USER_PROPERTY_NAME = "javax.persistence.jdbc.user";
     private static final String PASSWORD_PROPERTY_NAME = "javax.persistence.jdbc.password";
 
-    public static Map<String, String> getEnvVariable() {
-        var dbUrlValue = getenv(DB_URL_ENV_NAME);
+    static Map<String, String> getEnvVariable() {
+        var dbUrlValue = getenv(DB_URL_ENV_NAME) + URL_PARAMETERS;
         var userValue = getenv(USER_ENV_NAME);
         var passwordValue = getenv(PASSWORD_ENV_NAME);
 
